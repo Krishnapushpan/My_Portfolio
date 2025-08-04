91c561bb3f9ecbfd2f394a2ef2aac6ad68079bd4
Krishnapushpan <krishnapushpan2003@gmail.com>
2025-08-04 10:06:16 +0530

    updated

--- My_port/src/pages/About.jsx
+++ My_port/src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => (
     <div className="flex-1">
       <h2 className="text-4xl font-bold text-gray-800 mb-4">About Me</h2>
       <p className="text-xl text-gray-700 mb-4">Hi, I'm Krishna Pushpan. I'm a passionate MERN stack developer with a strong foundation in web development and blockchain. I love building dynamic web apps and learning new technologies. My goal is to create user-friendly solutions that make a difference.</p>
-      <p className="text-gray-500 mb-6">Currently, I'm working as a MERN Stack Developer at the Incubation Center, Malabar College of Engineering and Technology, Thrissur. I enjoy working with React, Node.js, Mongodb and exploring new tech trends.</p>
+      <p className="text-gray-500 mb-6">Currently, I'm working as a MERN Stack Developer Trainee at Adam Finastra Pvt.Ltd, Incubation Centre, MCET Thrissur. I enjoy working with React, Node.js, Mongodb and exploring new tech trends.</p>
       <a href="/Krishna__Pushpan.pdf" download className="bg-gray-900 text-white px-6 py-3 rounded-lg font-semibold shadow hover:bg-gray-800 transition inline-block">Download CV</a>
     </div>
     {/* Image */}
